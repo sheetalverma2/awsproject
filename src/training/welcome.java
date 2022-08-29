a4f622e293188555a0a12bca5b4e54ec6700ccf5
anand <anand@DESKTOP-9JSPD2N.myfiosgateway.com>
2022-08-28 23:13:33 -0400

    my second commit for awsproject

--- src/training/welcome.java
+++ src/training/welcome.java
@@ -5,7 +5,7 @@ public class welcome {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the World of DevOps");
 		System.out.println(20 + 50);
-		
+		System.out.println("Program by Sheetal, Aruna and Moorty");
 		
 	}
 
